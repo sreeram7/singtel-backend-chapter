162646137c9e77cd3793e1650a547e56a2761b99
sreeram7 <mandasriram.7@gmail.com>
2022-08-29 18:31:23 +0800

    Shark and Clownfish which have size and color attributes

--- src/test/java/com/singtel/TestScenario2.java
+++ src/test/java/com/singtel/TestScenario2.java
@@ -2,6 +2,8 @@ package com.singtel;
 
 import com.singtel.actions.Singer;
 import com.singtel.actions.Walker;
+import com.singtel.enums.Color;
+import com.singtel.enums.Size;
 import com.singtel.model.Clownfish;
 import com.singtel.model.Dolphin;
 import com.singtel.model.Fish;
@@ -15,12 +17,10 @@ import static org.junit.Assert.assertTrue;
 
 @Slf4j
 public class TestScenario2 {
-
     Fish fish;
     Shark shark;
     Clownfish clownfish;
     Dolphin dolphin;
-
     @Before
     public void setUp() throws Exception {
         fish = new Fish();
@@ -28,7 +28,6 @@ public class TestScenario2 {
         clownfish = new Clownfish("Clownfish");
         dolphin = new Dolphin();
     }
-
     @After
     public void tearDown() throws Exception {
         fish = null;
@@ -36,23 +35,46 @@ public class TestScenario2 {
         clownfish = null;
         dolphin = null;
     }
-
     // Test B1
     @Test
     public void testFishDoesNotSing() {
         log.info("--- testFishDoesNotSing ---");
         assertTrue(!(fish instanceof Singer));
     }
-
     @Test
     public void testFishDoesNotWalk() {
         log.info("--- testFishDoesNotWalk ---");
         assertTrue(!(fish instanceof Walker));
     }
-
     @Test
     public void testFishSwims() {
         log.info("--- testFishCannotSing ---");
         assertTrue(fish.swim());
+    }
+
+    @Test
+    public void testSharkIsLargeAndGrey() {
+        log.info("--- testSharkIsLargeAndGrey ---");
+        assertTrue(shark.getColor().equals(Color.GREY));
+        assertTrue(shark.getSize().equals(Size.LARGE));
+    }
+
+    @Test
+    public void testClownfishIsSmallAndOrange() {
+        log.info("--- testClownfishIsSmallAndOrange ---");
+        assertTrue(clownfish.getColor().equals(Color.ORANGE));
+        assertTrue(clownfish.getSize().equals(Size.SMALL));
+    }
+
+    @Test
+    public void testClownfishMakesJokes() {
+        log.info("--- testClownfishMakesJokes ---");
+        assertTrue(clownfish.makeJokes());
+    }
+
+    @Test
+    public void testSharkEatsOtherFish() {
+        log.info("--- testSharkEatsOtherFish ---");
+        assertTrue(shark.prey());
     }
 }
