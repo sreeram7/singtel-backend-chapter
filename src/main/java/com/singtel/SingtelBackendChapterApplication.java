5074bf2ddeae3d42aab4810156b0ed2f0be38d99
sreeram7 <mandasriram.7@gmail.com>
2022-08-29 12:08:24 +0800

    initial commit

---
+++ src/main/java/com/singtel/SingtelBackendChapterApplication.java
@@ -0,0 +1,16 @@
+package com.singtel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class SingtelBackendChapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingtelBackendChapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singtel/SingtelBackendChapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singtel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingtelBackendChapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
